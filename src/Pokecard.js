9cdad5d35a8f214bbd44f85b8854b5478658007c
Jon K <Johnakao@gmail.com>
2021-04-05 15:49:18 -0700

    Final Pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,15 @@
+import './Pokecard.css';
+
 function Pokecard(props) {
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`
   return (
-  <div>
-    <p>{props.name}</p>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png"></img>
-    <p>Type: {props.type}</p>
-    <p>EXP: {props.base_experience}</p>
-  </div>
+    <div className="pokecard">
+      <p>{props.name}</p>
+      <img src={ url }></img>
+      <p>Type: {props.type}</p>
+      <p>EXP: {props.base_experience}</p>
+    </div>
   )
 }
+
+export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,1 +1,20 @@
-function
+import Pokecard from "./Pokecard";
+import './Pokedex.css';
+
+function Pokedex() {
+  const pokeData = [
+    {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
+    {id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
+    {id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
+    {id: 12,  name: 'Butterfree', type: 'flying',   base_experience: 178},
+    {id: 25,  name: 'Pikachu',    type: 'electric', base_experience: 112},
+    {id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
+    {id: 94,  name: 'Gengar',     type: 'poison',   base_experience: 225},
+    {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
+  ];
+  const pokedex = pokeData.map(e => <Pokecard id={e.id} name={e.name} type={e.type} base_experience={e.base_experience} />);
+
+  return <div className="red-pokedex"> { pokedex } </div>
+}
+
+export default Pokedex; 
